8a92dbad499160cd2b34d1e65e247e5c8dfbcb4c
Georg <georg.strassberger@googlemail.com>
2023-05-07 16:05:37 +0200

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './modules/material.module';
+import { MaterialModule } from './shared/modules/material.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [BrowserAnimationsModule],
-  exports: [BrowserAnimationsModule],
+  imports: [BrowserAnimationsModule, MatToolbarModule],
+  exports: [BrowserAnimationsModule, MatToolbarModule],
 })
 export class MaterialModule {}
