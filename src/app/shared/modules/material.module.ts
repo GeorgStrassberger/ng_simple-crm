f4630d42900de28d7cc344640265b08d858bd862
Georg <georg.strassberger@googlemail.com>
2023-05-07 16:24:42 +0200

    add sidenav

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  imports: [BrowserAnimationsModule, MatToolbarModule],
-  exports: [BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
